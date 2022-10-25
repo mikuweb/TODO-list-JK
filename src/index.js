97da9d50e8a417c0d9ad23d0efc1ca749b56cce9
Miku Kawata <100392926+mikuweb@users.noreply.github.com>
2022-10-25 12:11:47 -0700

    Add function Add "add input" function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import "./styles.css";
+///////////////////
+// Get element
+const inputBtn = document.querySelector(".btn-input");
+const incompleteTodos = document.querySelector(".incomplete-todos");
+///////////////////
+// Function
+const addTodo = function (e) {
+  const inputText = document.querySelector(".text-input").value;
+  console.log(inputText);
+  document.querySelector(".text-input").value = "";
+
+  // Create li, div, p
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  div.className = "list-row";
+  const p = document.createElement("p");
+  p.className = "todo-item";
+  p.innerText = inputText;
+
+  // Organize directory
+  li.appendChild(div);
+  div.appendChild(p);
+
+  // Add to incomplete lists
+
+  incompleteTodos.appendChild(li);
+  console.log(li);
+};
+
+///////////////////
+// Event listener
+inputBtn.addEventListener("click", addTodo);
